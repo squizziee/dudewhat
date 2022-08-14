4b8e914a4947d052e2378aeba1ee5f9797023190
squizziee <3925181@gmail.com>
2022-08-14 16:35:13 +0300

    Basic functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
 let listItems = document.querySelectorAll(".list_item")
 
 for (let i = 0; i < listItems.length; i++) {
-    listItems[i].addEventListener("click", function() {
-        listItems[i].classList.add("list_item_crossed");
+    let obj = listItems[i];
+    obj.addEventListener("click", function() {
+        if (!obj.classList.contains("list_item_crossed"))
+            listItems[i].classList.add("list_item_crossed");
+        else
+            obj.classList.remove("list_item_crossed");
     })
 }
+
+let list = document.getElementById("list");
+let prompt = document.getElementById("prompt");
+let addBtn = document.getElementById("addBtn");
+let removeBtn = document.getElementById("removeBtn");
+
+addBtn.addEventListener("click", function() {
+    let listItem = document.createElement("div");
+    listItem.classList.add("list_item");
+
+    const node = document.createTextNode("- " + prompt.value);
+
+    listItem.appendChild(node);
+    list.appendChild(listItem);
+
+    prompt.value = "";
+})
+
+removeBtn.addEventListener("click", function () {
+    list.innerHTML = '';
+})
