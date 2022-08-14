4f8df06bea878240cb781d513fd945842c0163f6
squizziee <3925181@gmail.com>
2022-08-14 20:12:39 +0300

    Ticking tasks functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 let listItems = document.querySelectorAll(".list_item")
+let list = document.getElementById("list");
+let prompt = document.getElementById("prompt");
+let addBtn = document.getElementById("addBtn");
+let removeBtn = document.getElementById("removeBtn");
 
+function addListener(obj) {
+    obj.addEventListener("click", function() {
+        if (!obj.classList.contains("list_item_crossed"))
+            obj.classList.add("list_item_crossed");
+        else
+            obj.classList.remove("list_item_crossed");
+    })
+}
+//temprorary
 for (let i = 0; i < listItems.length; i++) {
     let obj = listItems[i];
     obj.addEventListener("click", function() {
@@ -10,20 +23,33 @@ for (let i = 0; i < listItems.length; i++) {
     })
 }
 
-let list = document.getElementById("list");
-let prompt = document.getElementById("prompt");
-let addBtn = document.getElementById("addBtn");
-let removeBtn = document.getElementById("removeBtn");
-
 addBtn.addEventListener("click", function() {
     let listItem = document.createElement("div");
     listItem.classList.add("list_item");
 
-    const node = document.createTextNode("- " + prompt.value);
+    let checkboxContainer = document.createElement("div");
+    checkboxContainer.classList.add("checkbox_container");
 
-    listItem.appendChild(node);
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("text_container");
+
+    let square = document.createElement("div");
+    square.classList.add("square");
+
+    let tick = document.createElement("i");
+    tick.classList.add("fa-solid");
+    tick.classList.add("fa-xmark");
+
+    let text = document.createTextNode(prompt.value);
+
+    textContainer.appendChild(text);
+    square.appendChild(tick);
+    checkboxContainer.appendChild(square);
+    listItem.appendChild(checkboxContainer);
+    listItem.appendChild(textContainer)
     list.appendChild(listItem);
-
+    
+    addListener(listItem);
     prompt.value = "";
 })
 
